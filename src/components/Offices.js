3a4a18257dd15620048546b2878b3bf948abd962
Mich <michola3636@gmail.com>
2021-03-13 14:47:37 +0000

    coms

--- src/components/Offices.js
+++ src/components/Offices.js
@@ -3,9 +3,13 @@ import "./InfoPages.css"
 
 function Offices() {
     return (
-        <div className="Infobody">
+        <div>
+        <div  className="Infobody"> 
         <h1>Offices</h1>
 
+       
+        </div>
+        <div className="bot-text"> 
         <p>
         everage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
 
@@ -13,6 +17,9 @@ Bring to the table win-win survival strategies to ensure proactive domination. A
 
 Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.
         </p>
+
+
+        </div>
         
     </div>
     )
