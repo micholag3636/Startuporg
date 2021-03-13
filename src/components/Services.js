aeaed9b0a4a0bf00e8461fd4b8bc489c18e2374a
Mich <michola3636@gmail.com>
2021-03-13 23:56:18 +0000

    git

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,11 +11,9 @@ function Services() {
      
             <Productscreen title="Web Hosting" text="We supply amazon web services for our clients" img="https://www.elegantthemes.com/blog/wp-content/uploads/2020/01/difference-between-domain-and-web-hosting-featured-image.jpg"/>
             <Productscreen title="Data Migration" text="We supply amazon web services for our clients" img="https://cdn.technologyadvice.com/wp-content/uploads/2017/05/Fotolia_130308566_Subscription_Monthly_M-700x408.jpg"/>
-            <Productscreen title="Infrastructure As A Service" text="We supply amazon web services for our clients" img="https://techfest.org/2020/workshops/img/w6.jpg"/>
+            <Productscreen title="Infrastructure As A Service" text="We supply amazon web services for our clients" img="https://www.channelnews.com.au/wp-content/uploads/2020/06/what_is_laaS_800x420.jpg"/>
             <Productscreen title="Cloud Security" text="We supply amazon web services for our clients" img="https://miro.medium.com/max/512/1*DQxb51_P6QQRSyTGMrxfBQ.jpeg"/>
             <Productscreen title="VoIP Services" text="We supply amazon web services for our clients" img="https://www.wpbeginner.com/wp-content/uploads/2019/06/voipforbusiness.png"/>
-            <Productscreen title="Amazon Web Services" text="We supply amazon web services for our clients" img="https://techfest.org/2020/workshops/img/w6.jpg"/>
-            <Productscreen title="Amazon Web Services" text="We supply amazon web services for our clients" img="https://techfest.org/2020/workshops/img/w6.jpg"/>
             <Productscreen title="Amazon Web Services" text="We supply amazon web services for our clients" img="https://techfest.org/2020/workshops/img/w6.jpg"/>
 
       
